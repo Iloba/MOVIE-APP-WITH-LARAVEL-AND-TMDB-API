0d70dde386f8bc9dfca22bb40fa397c1c3851f72
Iloba <scarlettsney@gmail.com>
2021-07-31 10:11:41 +0100

    search

--- resources/views/livewire/search-drop-down.blade.php
+++ resources/views/livewire/search-drop-down.blade.php
@@ -4,6 +4,10 @@
         <div class="absolute top-0">
             <i style="font-size: 12px;" class="icofont icofont-search fill-current text-gray-500   ml-3 "></i>
         </div>
+        <div wire:loading class="spinner top-0 right-0 mr-4 mt-4">
+    </div>
+    
+
     </div>
     @if (strlen($search) > 2)
     <div class="absolute bg-gray-800 rounded w-64 mt-4 text-sm">
@@ -12,7 +16,13 @@
             @foreach ($searchResults as $result)
                 <li class="border-b border-gray-700">
                     <a class="block hover:bg-gray-700 px-3 py-3 flex items-center" href="{{route('movies.show', $result['id'])}}">
-                        <span>{{$result['title']}}</span>
+                        @if ($result['poster_path'])
+                             <span><img src="https://image.tmdb.org/t/p/w92/{{$result['poster_path']}}" class="w-8" alt="movie"></span>
+                        @else
+                        <span><img src="https://image.tmdb.org/t/p/w92/{{$result['poster_path']}}" class="w-8" alt="movie"></span>
+                        @endif
+                       
+                        <span class="ml-4">{{$result['title']}}</span>
                     </a>
                 </li>
             @endforeach
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,21 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    spinner: (theme) => ({
+      default: {
+        color: '#dae1e7', // color you want to make the spinner
+        size: '1em', // size of the spinner (used for both width and height)
+        border: '2px', // border-width of the spinner (shouldn't be bigger than half the spinner's size)
+        speed: '500ms', // the speed at which the spinner should rotate
+      },
+     
+    }),
+   
   },
   variants: {
-    extend: {},
+    spinner: ['responsive'],
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-spinner')({ className: 'spinner', themeKey: 'spinner' }),
+  ],
 }
